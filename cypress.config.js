1761ebbcfc846b4773f244147db0701d3c19f439
ctkdev3256 <chris_thomas@gmail.com>
2024-06-25 09:54:34 -0700

    modified .json,.yml,.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
-viewportHeight: 860,
-viewportWidth:1224,
-chromeWebSecurity: false,
+  defaultCommandTimeout: 5000,
+  viewportHeight: 860,
+  viewportWidth: 1224,
+  chromeWebSecurity: false,
 
   e2e: {
     baseUrl: 'https://www.republicservices.com',
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
-     
+
       // implement node event listeners here
     },
   },
